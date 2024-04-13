635ce6d063ba330dd043bdf1127b333902b3d057
Virtuouz <giovanni_plascencia@hotmail.com>
2024-04-13 14:07:16 -0600

    fix image shortcode

--- .eleventy.js
+++ .eleventy.js
@@ -4,46 +4,57 @@ const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
 const rssPlugin = require("@11ty/eleventy-plugin-rss");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const markdownIt = require('markdown-it')
-const markdownItAnchor = require('markdown-it-anchor')
-const pluginTOC = require('eleventy-plugin-toc')
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const pluginTOC = require("eleventy-plugin-toc");
 const pluginBookshop = require("@bookshop/eleventy-bookshop");
 const yaml = require("js-yaml");
 const { execSync } = require("child_process");
-
-
+const fs = require("fs");
 
 const imageShortcode = async (
   src,
   alt,
-  cls="",  
+  cls = "",
   sizes = "100vw",
-  widths = [200, 400, 850, 1280,1920, 2500, null],
-  formats = ["avif", "webp", "svg", "jpeg"]
+  widths = [200, 400, 850, 1280, 1600],
+  formats = ["avif", "webp", "svg", "jpeg"],
 ) => {
-  const imageMetadata = await Image(src, {
-    widths: [...widths],
-    formats: [...formats, null],
-    outputDir: "dist/assets/images",
-    urlPath: "/assets/images",
-  });
+  let before = Date.now();
+  let inputFilePath =
+    src == null ? src : path.join("src", src);
 
-  const imageAttributes = {
-    class: cls,
-    alt,
-    sizes,
-    loading: "lazy",
-    decoding: "async",
-  };
+  if (fs.existsSync(inputFilePath)) {
+    
+    console.log(
+      `[11ty/eleventy-img] ${Date.now() - before}ms: ${inputFilePath}`,
+    );
+    const imageMetadata = await Image(inputFilePath, {
+      widths: [...widths],
+      formats: [...formats, null],
+      outputDir: "dist/assets/images",
+      urlPath: "/assets/images",
+    });
 
-  return Image.generateHTML(imageMetadata, imageAttributes);
+    const imageAttributes = {
+      class: cls,
+      alt,
+      sizes: sizes || "100vw",
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    return Image.generateHTML(imageMetadata, imageAttributes);
+  } else{
+    return `<img class='${cls}' src='${src}' alt='${alt}'>`;
+  }
 };
 
 function generateImages(src, widths = [200, 400, 850, 1920, 2500]) {
   let source = src;
   let options = {
-    widths: [...widths,null],
-    formats: ["jpeg","webp", null],
+    widths: [...widths, null],
+    formats: ["jpeg", "webp", null],
     outputDir: "dist/assets/images",
     urlPath: "/assets/images",
     useCache: true,
@@ -66,7 +77,7 @@ function imageCssBackground(src, selector, widths) {
   // i use always jpeg for backgrounds
   metadata.webp.slice(1).forEach((image, idx) => {
     markup.push(
-      `@media (min-width: ${metadata.jpeg[idx].width}px) { ${selector} {background-image: url(${image.url});}}`
+      `@media (min-width: ${metadata.jpeg[idx].width}px) { ${selector} {background-image: url(${image.url});}}`,
     );
   });
   return markup.join("");
@@ -74,14 +85,13 @@ function imageCssBackground(src, selector, widths) {
 
 module.exports = (eleventyConfig) => {
   // Markdown
-  eleventyConfig.setLibrary(
-    'md',
-    markdownIt().use(markdownItAnchor)
-  )
+  eleventyConfig.setLibrary("md", markdownIt().use(markdownItAnchor));
 
-  eleventyConfig.on('eleventy.after', ()=>{
-    execSync('npx tailwindcss -i ./src/css/styles.css -o ./dist/css/styles.css --minify')
-  })
+  eleventyConfig.on("eleventy.after", () => {
+    execSync(
+      "npx tailwindcss -i ./src/css/styles.css -o ./dist/css/styles.css --minify",
+    );
+  });
 
   //eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
   //eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
@@ -91,30 +101,31 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy("./src/_includes/partials/background");
   // eleventyConfig.addPassthroughCopy("./src/css/");
-  eleventyConfig.addPassthroughCopy({"./src/images/favicon":"/"})
+  eleventyConfig.addPassthroughCopy({ "./src/images/favicon": "/" });
   eleventyConfig.addPassthroughCopy("./src/fonts");
   //eleventyConfig.addPassthroughCopy({ './src/robots.txt': '/robots.txt' });
-  eleventyConfig.addPassthroughCopy('./src/_redirects');
+  eleventyConfig.addPassthroughCopy("./src/_redirects");
   eleventyConfig.addAsyncShortcode("image", imageShortcode);
   eleventyConfig.addShortcode("cssBackground", imageCssBackground);
   eleventyConfig.addPlugin(rssPlugin);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPlugin(pluginTOC,{tags:['h1','h2','h3','h4']})
+  eleventyConfig.addPlugin(pluginTOC, { tags: ["h1", "h2", "h3", "h4"] });
 
-  eleventyConfig.addPlugin(pluginBookshop({
-    bookshopLocations: ["_component-library"],  
-    pathPrefix: '', 
-  }));
+  eleventyConfig.addPlugin(
+    pluginBookshop({
+      bookshopLocations: ["_component-library"],
+      pathPrefix: "",
+    }),
+  );
 
   // Returns a collection of blog posts in reverse date order
   eleventyConfig.addCollection("blog", (collection) => {
     return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
   });
 
-  eleventyConfig.addCollection("pages", (collection)=>{
-    return collection.getFilteredByGlob("./src/pages/**/*.md")
+  eleventyConfig.addCollection("pages", (collection) => {
+    return collection.getFilteredByGlob("./src/pages/**/*.md");
   });
-
 
   eleventyConfig.addFilter("dateFilter", dateFilter);
   eleventyConfig.addFilter("w3DateFilter", w3DateFilter);
@@ -127,7 +138,7 @@ module.exports = (eleventyConfig) => {
     cssTemplateEngine: "liquid",
     dir: {
       input: "src",
-      pages:'pages',
+      pages: "pages",
       output: "dist",
     },
   };
