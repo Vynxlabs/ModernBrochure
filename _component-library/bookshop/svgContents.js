ca2563eca835b7b1211e9bbadb6354e1361f8150
Virtuouz <giovanni_plascencia@hotmail.com>
2024-05-26 17:25:06 -0600

    remove svg contents filter

--- _component-library/bookshop/svgContents.js
+++
@@ -1,9 +0,0 @@
-const GetSVGContents = require("eleventy-plugin-svg-contents").GetSVGContents;
-
-module.exports = function (Liquid) {
-  this.registerFilter('svgContents', (file, className, extractTag = 'svg') => {
-    const getSVGContents = new GetSVGContents(file, className, extractTag);
-
-    return getSVGContents.getSvg();
-  });
-};
