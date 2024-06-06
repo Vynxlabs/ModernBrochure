98ad06d6e530a84fbc5a9124a928d97ac319b789
Virtuouz <giovanni_plascencia@hotmail.com>
2024-06-05 21:40:52 -0600

    update services

--- _component-library/bookshop/getServicesCategories.js
+++ _component-library/bookshop/getServicesCategories.js
@@ -4,6 +4,7 @@ module.exports = function (Liquid) {
     this.registerFilter('categoriesFilter', (collection) => {
         const CATEGORIES = new Map();
         for (const item of collection) {
+            if (item.data.draft) continue;
             const slug = slugify(item.data.category ? item.data.category : 'Services', { lower: true });
             if (!CATEGORIES.has(slug)) {
                 CATEGORIES.set(slug, item.data.category ? item.data.category : 'Services');
