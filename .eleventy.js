18453cad0571d0f9129fe7320cbdc26d4d08658e
Virtuouz <giovanni_plascencia@hotmail.com>
2024-11-12 19:40:27 -0700

    add pathExists filter

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,7 @@ const stringifyFilter = require("./src/filters/stringify-filter.js");
 const evalLiquid = require("./src/filters/evalLiquid-filter.js");
 const happeningsFilter = require("./src/filters/happenings-filter.js");
 const getServiceCategories = require("./src/filters/getServiceCategories-filter.js");
+const pathExistsFilter = require("./src/filters/pathExists-filter.js");
 const rssPlugin = require("@11ty/eleventy-plugin-rss");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it"),
@@ -275,6 +276,7 @@ module.exports = (eleventyConfig) => {
   });
   eleventyConfig.addFilter("evalLiquid", evalLiquid);
   eleventyConfig.addFilter("happeningsFilter", happeningsFilter);
+  eleventyConfig.addFilter("pathExists", pathExistsFilter);
 
   eleventyConfig.on("eleventy.before", () => {
     execSync("node ./utils/syncPermalinks.js");
---
+++ src/filters/pathExists-filter.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (filePath) => {
+  try {
+    // Resolve the full path based on the current working directory
+    const fullPath = path.resolve("./src",filePath);
+
+    // Check if the file exists
+    if (fs.existsSync(fullPath)) {
+      return filePath; // Return the original path if the file exists
+    } else {
+      return ""; // Return an empty string if the file does not exist
+    }
+  } catch (error) {
+    console.error(`Error checking file existence for path: ${filePath}`, error);
+    return ""; // Return empty string on any error
+  }
+};
