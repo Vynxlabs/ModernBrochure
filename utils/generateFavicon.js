773265d8f72653d54b4eb1c8eafca11983a93268
Virtuouz <giovanni_plascencia@hotmail.com>
2024-11-16 21:58:49 -0700

    update favicon generation code

--- utils/generateFavicon.js
+++ utils/generateFavicon.js
@@ -1,55 +1,88 @@
-const { FaviconSettings, MasterIcon, generateFaviconFiles, generateFaviconHtml } = require('@realfavicongenerator/generate-favicon');
-const { getNodeImageAdapter, loadAndConvertToSvg } = require('@realfavicongenerator/image-adapter-node');
-const fs = require('fs');
-const path = require('path');
+const {
+  FaviconSettings,
+  MasterIcon,
+  generateFaviconFiles,
+  generateFaviconHtml,
+} = require("@realfavicongenerator/generate-favicon");
+const {
+  getNodeImageAdapter,
+  loadAndConvertToSvg,
+} = require("@realfavicongenerator/image-adapter-node");
+const fs = require("fs");
+const path = require("path");
+const { compileString } = require("sass");
 
-const imageAdapter = await getNodeImageAdapter();
+async function generateFavicon() {
+  const imageAdapter = await getNodeImageAdapter();
 
-// This is the icon that will be transformed into the various favicon files
-const masterIcon = {
-  icon: await loadAndConvertToSvg("path/to/master-icon.svg"),
+  // Load the master icon
+  const masterIcon = {
+    icon: await loadAndConvertToSvg("src/assets/uploads/vynxlabs-08.png"),
+  };
+
+  const faviconSettings = {
+    icon: {
+      desktop: {
+        regularIconTransformation: {
+          type: "none",
+        },
+        darkIconType: "none",
+      },
+      touch: {
+        transformation: {
+          type: "none",
+        },
+        appTitle: null,
+      },
+      webAppManifest: {
+        transformation: {
+          type: "none",
+        },
+        backgroundColor: "#ffffff",
+        themeColor: "#ffffff",
+        name: "MyWebSite",
+        shortName: "MySite",
+      },
+    },
+    path: "/",
+  };
+
+  const faviconFolder = path.join(process.cwd(), "src/images/favicon");
+  fs.rmSync(faviconFolder, { recursive: true, force: true });
+  fs.mkdirSync(faviconFolder);
+
+  // Generate files
+  const files = await generateFaviconFiles(
+    masterIcon,
+    faviconSettings,
+    imageAdapter,
+  );
+
+  for (const fileName in files) {
+    const filePath = path.join(faviconFolder, fileName);
+    const fileContents = files[fileName];
+
+    // Check the type of fileContents
+    if (Buffer.isBuffer(fileContents) || typeof fileContents === "string") {
+      fs.writeFileSync(filePath, fileContents);
+    } else if (typeof fileContents === "object") {
+      fs.writeFileSync(filePath, JSON.stringify(fileContents, null, 2));
+    } else {
+      console.warn(`Unsupported file type for ${fileName}`);
+    }
+  }
+
+ const html = await generateFaviconHtml(faviconSettings);
+
+  // Extract markups and join them into a single HTML string
+  const htmlContent = html.markups.join("\n");
+  console.log(htmlContent)
+  const htmlFilePath = path.join(
+    process.cwd(),
+    "src/_includes/partials/favicon.html",
+  );
+
+  fs.writeFileSync(htmlFilePath, htmlContent); 
 }
 
-const faviconSettings = {
-  icon: {
-    desktop: {
-      regularIconTransformation: {
-        type: IconTransformationType.None,
-      },
-      darkIconType: "none",
-    },
-    touch: {
-      transformation: {
-        type: IconTransformationType.None,
-      },
-      appTitle: null
-    },
-    webAppManifest: {
-      transformation: {
-        type: IconTransformationType.None,
-      },
-      backgroundColor: "#ffffff",
-      themeColor: "#ffffff",
-      name: "MyWebSite",
-      shortName: "MySite"
-    }
-  },
-  path: "/",
-};
-
-// Remove existing files in src/images/favicon
-const faviconFolder = path.join(__dirname, 'src/images/favicon');
-fs.rmSync(faviconFolder, { recursive: true, force: true });
-fs.mkdirSync(faviconFolder);
-
-// Generate files
-const files = await generateFaviconFiles(masterIcon, faviconSettings, imageAdapter);
-files.forEach((file) => {
-  const filePath = path.join(faviconFolder, file.name);
-  fs.writeFileSync(filePath, file.contents);
-});
-
-// Generate HTML
-const html = await generateFaviconHtml(faviconSettings);
-const htmlFilePath = path.join(__dirname, 'src/_includes/partials/favicon.html');
-fs.writeFileSync(htmlFilePath, html);
+generateFavicon();
