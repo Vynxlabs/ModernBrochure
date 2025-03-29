e4bf61461ba83a776c128d5545af3d0962a6eaf0
Virtuouz <giovanni_plascencia@hotmail.com>
2025-03-29 09:42:06 -0600

    update listnig default layout

--- src/listings/listingis.11tydata.js
+++ src/listings/listingis.11tydata.js
@@ -11,7 +11,7 @@ function showDraft(data) {
 }
 
 module.exports = {
-	layout: "./layouts/base.html",
+	layout: "./layouts/page.html",
 	eleventyComputed: {
 		eleventyExcludeFromCollections: function(data) {
 			if (showDraft(data)) {
