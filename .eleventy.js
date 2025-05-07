5cb70a7feb1604754b193927baa546830cd0a047
Virtuouz <giovanni_plascencia@hotmail.com>
2025-05-06 20:03:27 -0600

    add js to remove lqip

--- .eleventy.js
+++ .eleventy.js
@@ -97,6 +97,7 @@ const imageShortcode = async (
   const imageAttributes = {
     class: cls,
     alt,
+    _: "on load js requestAnimationFrame (() => requestAnimationFrame(() => {return})) end then set my @style to ''",
     style: imageHashes[Image.getHash(inputFilePath)],
     sizes: sizes || "100vw",
     loading: "lazy",
